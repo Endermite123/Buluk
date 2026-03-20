1e6804d2633aecd6d4405c9039d6adcd32531f67
Endermite123 <a01802168@gmail.com>
2026-03-19 18:59:11 -0600

    Main bien

--- MARS base/Documentation/Guide/Tests.md
+++ MARS base/Documentation/Guide/Tests.md
@@ -6,3 +6,4 @@
 Parameters: name
 
 The `@MARSTEST` annotation is used to define a test case for a MARS module. The `name` parameter specifies the name of the test case.
+
--- MARS base/Template/src/main/java/frc/tests/EmptyTest.java
+++ MARS base/Template/src/main/java/frc/tests/EmptyTest.java
@@ -7,30 +7,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 
-/**
- * <b> @MARSTest </b>
- * Annotation used to register a {@link TestRoutine} in the MARS system.
- *
- * <p>Any class annotated with {@code @MARSTest} will be automatically
- * discovered and made available to run during test mode.
- *
- * <ul>
- *   <li>The class must extend {@link TestRoutine}</li>
- *   <li>The class must implement {@link TestRoutine#getRoutineCommand()}</li>
- * </ul>
- *
- * <p><b>Parameters:</b>
- * <ul>
- *   <li><b>name</b>: A unique name used to identify the test</li>
- * </ul>
- *
- * <p><b>How it works:</b>
- * <ol>
- *   <li>MARS scans the code for this annotation</li>
- *   <li>Registers all test routines</li>
- *   <li>Allows selecting and running them</li>
- * </ol>
- */
 
 
 @MARSTest(name = "NONE")
@@ -42,3 +18,4 @@ public class EmptyTest extends TestRoutine{
         return Commands.none();
     }
 }
+
